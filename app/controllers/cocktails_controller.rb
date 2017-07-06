a1de8246bf274df1d0ced3280f01c22a238c2dfb
Jessica Hines <jessicahines717@gmail.com>
2017-07-06 14:14:31 -0400

    made basic pages and forms for testing purposes

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,30 +1,52 @@
 class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [ :show, :edit, :update, :destroy ]
+
   def index
+    @cocktails = Cocktail.all
   end
 
   def show
   end
 
   def new
+    @cocktail = Cocktail.new
   end
 
   def create
+    Cocktail.create(cocktail_params)
+    redirect_to cocktails_path(@cocktail)
   end
 
   def edit
   end
 
   def update
+    respond_to do |format|
+      if @restaurant.update(restaurant_params)
+        format.html { redirect_to @restaurant, notice: 'Restaurant was successfully updated.' }
+        format.json { render :show, status: :ok, location: @restaurant }
+      else
+        format.html { render :edit }
+        format.json { render json: @restaurant.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @cocktail.destroy
+    respond_to do |format|
+      format.html { redirect_to restaurants_path, notice: 'Restaurant was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
 
   def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
   end
 
   def cocktail_params
+    params.require(:cocktail).permit(:name)
   end
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,7 +1,20 @@
 class DosesController < ApplicationController
   def new
+    @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
   end
 
   def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    @dose.save!
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description)
   end
 end
