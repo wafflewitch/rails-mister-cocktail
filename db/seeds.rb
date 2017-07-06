ed445c569a1ba60e3da489242f399cb68f1f6a8d
Jessica Hines <jessicahines717@gmail.com>
2017-07-06 13:19:58 -0400

    seeded ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+singular = ["unicorn", "vampire", "mermaid", "centaur", "goblin", "werewolf", "wizard", "sugar", "chocolate", "caramel", "hazelnut", "vanilla", "strawberry", "awesome", "rainbow", "happiness", "sadness"]
+plural = ["blood", "tears", "breath", "essence", "sauce", "flavoring", "sprinkles", "powder", "magic"]
+
+# 25.times do
+#   ingredient = Ingredient.create(name: "#{singular.sample} #{plural.sample}")
+#   p ingredient
+# end
+
+# ingredient = Ingredient.create(name: "awesome sauce")
+# p ingredient
+# ingredient = Ingredient.create(name: "unicorn tears")
+# p ingredient
+# ingredient = Ingredient.create(name: "vampire blood")
+# p ingredient
+# ingredient = Ingredient.create(name: "magic sprinkles")
+# p ingredient
+# ingredient = Ingredient.create(name: "magic powder")
+# p ingredient
+# ingredient = Ingredient.create(name: "vanilla essence")
+# p ingredient
+# ingredient = Ingredient.create(name: "vanilla flavoring")
+# p ingredient
+# ingredient = Ingredient.create(name: "caramel sauce")
+# p ingredient
+# ingredient = Ingredient.create(name: "caramel flavoring")
+# p ingredient
+# ingredient = Ingredient.create(name: "rainbow sprinkles")
+# p ingredient
+# ingredient = Ingredient.create(name: "rainbow magic")
+# p ingredient
+# ingredient = Ingredient.create(name: "rainbow sauce")
+# p ingredient
