cd2a107bbe4e0c91dc83fcb00cd30ab2dee59c96
Jessica Hines <jessicahines717@gmail.com>
2017-07-06 13:35:40 -0400

    added controllers and routes

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,30 @@
+class CocktailsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_cocktail
+  end
+
+  def cocktail_params
+  end
+end
