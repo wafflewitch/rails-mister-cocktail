b984cff3002f6ab3c55a4a489edf40d147f4d623
Jessica Hines <jessicahines717@gmail.com>
2017-07-07 11:06:28 -0400

    added more css for form

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -5,13 +5,4 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
 
   DOSES = ["a pinch of", "a dash of", "a spoonful of", "a dollop of", "a whole lotta"]
-  # DOSES_HASH = {
-  #   1: "a pinch of",
-  #   2: "a dash of",
-  #   3: "a spoonful of",
-  #   4: "a dollop of",
-  #   5: "a whole lotta"
-  # }
-
-
 end
