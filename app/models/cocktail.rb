c45be04f292cfac8cdd2b652f927d951c7d2a0ba
Jessica Hines <jessicahines717@gmail.com>
2017-07-07 20:55:45 -0400

    put comments on cocktail show page

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,5 +2,6 @@ class Cocktail < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :ingredients, through: :doses
   has_many :doses, dependent: :destroy
+  has_many :reviews, dependent: :destroy
   mount_uploader :photo, PhotoUploader
 end
